0f019e95b7e7b3daa3b1ea4077173fee75c7a0b2
Александра <119693862+AleksandraZgonyaiko@users.noreply.github.com>
2022-12-05 19:27:07 +0300

    Add files via upload

---
+++ TestProject/Forms/ZgonyaikoForm.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace TestProject.Forms
+{
+    partial class ZgonyaikoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.infoLabel1 = new System.Windows.Forms.Label();
+            this.infoLabel2 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.newsLabel = new System.Windows.Forms.Label();
+            this.weatherLabel = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.cancelButton);
+            this.flowLayoutPanel1.Controls.Add(this.okButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 418);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 32);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(722, 3);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Отмена";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(641, 3);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 0;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.nameLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ageLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.nameTextBox, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ageTextBox, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.infoLabel1, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.infoLabel2, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.newsLabel, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.weatherLabel, 5, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 418);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nameLabel.Location = new System.Drawing.Point(3, 0);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(52, 26);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Имя:";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ageLabel.Location = new System.Drawing.Point(3, 26);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(52, 26);
+            this.ageLabel.TabIndex = 1;
+            this.ageLabel.Text = "Возраст:";
+            this.ageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // infoLabel1
+            // 
+            this.infoLabel1.AutoSize = true;
+            this.infoLabel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.infoLabel1.Location = new System.Drawing.Point(167, 0);
+            this.infoLabel1.Name = "infoLabel1";
+            this.infoLabel1.Size = new System.Drawing.Size(94, 26);
+            this.infoLabel1.TabIndex = 2;
+            this.infoLabel1.Text = "Новости в мире: ";
+            this.infoLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // infoLabel2
+            // 
+            this.infoLabel2.AutoSize = true;
+            this.infoLabel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.infoLabel2.Location = new System.Drawing.Point(167, 26);
+            this.infoLabel2.Name = "infoLabel2";
+            this.infoLabel2.Size = new System.Drawing.Size(94, 26);
+            this.infoLabel2.TabIndex = 3;
+            this.infoLabel2.Text = "Погода: ";
+            this.infoLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(61, 3);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Location = new System.Drawing.Point(61, 29);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ageTextBox.TabIndex = 5;
+            // 
+            // newsLabel
+            // 
+            this.newsLabel.AutoSize = true;
+            this.newsLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.newsLabel.Location = new System.Drawing.Point(267, 0);
+            this.newsLabel.Name = "newsLabel";
+            this.newsLabel.Size = new System.Drawing.Size(530, 26);
+            this.newsLabel.TabIndex = 6;
+            this.newsLabel.Text = "Все хорошо";
+            this.newsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // weatherLabel
+            // 
+            this.weatherLabel.AutoSize = true;
+            this.weatherLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.weatherLabel.Location = new System.Drawing.Point(267, 26);
+            this.weatherLabel.Name = "weatherLabel";
+            this.weatherLabel.Size = new System.Drawing.Size(530, 26);
+            this.weatherLabel.TabIndex = 7;
+            this.weatherLabel.Text = "Солнечно";
+            this.weatherLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ZgonyaikoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "ZgonyaikoForm";
+            this.Text = "ZgonyaikoForm";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.Label infoLabel1;
+        private System.Windows.Forms.Label infoLabel2;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.Label newsLabel;
+        private System.Windows.Forms.Label weatherLabel;
+    }
+}
